509567801f49e78a22dd03b4d46f7d51ab4a9d47
The Malik of Codes <ahmadmalaysia2000@gmail.com>
2021-09-11 11:11:22 +0100

    Add files via upload

---
+++ 6) Working with Number.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << 40      << endl;
+	cout << 40 + 35 << endl;
+	cout << 40 - 15 << endl;
+	cout << 45/5    << endl;
+	cout << 40*30   << endl;
+	cout << 40 % 3  << endl;
+	// BODMAS
+	cout << 4 * 7 +10 << endl;
+	cout << (40 + 5) * 10<< endl;
+	cout << "\n" << endl;
+	
+	int wnum = 6;
+	double dnum = 3.6;
+	
+	wnum++;
+	cout << wnum << endl;
+	
+	wnum --;
+	cout << wnum << endl;
+	
+	wnum += 80;
+	cout << wnum << endl;
+	
+	wnum /= 80;	
+	cout << wnum << endl;
+	
+	//
+	cout << 100 + 50.5  << endl;
+	cout << 156 - 50.5  << endl;
+	cout << 50*3  << endl;
+	cout << 100 + 50.5  << endl;
+	cout << "\n" << endl;
+	
+	cout <<  20 / 3.0 << endl;
+	return 0;
+}
