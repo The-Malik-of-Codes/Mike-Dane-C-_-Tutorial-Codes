9b6a95e31c2d5cc62ef2b1f2e0c5b081de71692f
The Malik of Codes <ahmadmalaysia2000@gmail.com>
2021-09-11 23:59:15 +0100

    Add files via upload

---
+++ 6)_Working_with_Number(a).cpp
@@ -0,0 +1,17 @@
+//58.04
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	cout << pow(2, 6);
+	cout << sqrt(36);
+	cout << sqrt(80)
+	cout << pow(2, 6);
+	cout << round(6.3);
+	cout << pow(2, 6);
+	
+	return 0;
+}
